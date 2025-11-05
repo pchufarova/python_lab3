d3d1598342c5831911e00bea667787c97c897379
Polina <chufarovap05@mail.ru>
2025-11-05 19:02:34 +0300

    исправлены константы

--- main.py
+++ main.py
@@ -5,9 +5,34 @@ from threading import Thread
 from PIL import Image
 import time
 
+HEIGHT = 500 # размеры окна
+WIDTH = 600
+
+LBL_HEIGHT = 40 # размеры лейбла где будет ключ
+LBL_WIDTH = 200
+LBL_PAD_Y = 90 # отступ для лейбла сверху
+
+GIF_HEIGHT = 135 # размеры гиф
+GIF_WIDTH = 185
+GIF_PAD_X = 2 # отступы для нее
+gif_pad_y = HEIGHT - GIF_HEIGHT - GIF_PAD_X
+
+BTN_HEIGHT = 50 # размеры кнопки
+BTN_WIDTH = 150
+btn_pad_y = gif_pad_y # отступ для кнопки сверху
+
+TEXT_PAD_X = 120
+TEXT_PAD_Y = 10 # отступы для текста 
+
+ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+GIF_FILE = "punchyandbob.gif" 
+ICON_FILE = "acnh_bells.png"
+MUSIC_FILE = "bubblegum k.k..mp3"
+BG_FILE = "acnh_bg.png"
+EFFECT_SOUND_FILE = "effect.mp3"
+
 
 def generate_key():
-    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     first_num = 0
     scnd_num = 0
     while scnd_num - first_num < 2: # проверяем, чтобы буквы не равнялись 
@@ -23,31 +48,13 @@ def generate_key():
     scnd_num += 1
     frst_prt = str(first_num) if len(str(first_num)) == 2 else f'0{first_num}'
     last_prt = str(scnd_num) if len(str(scnd_num)) == 2 else f'0{scnd_num}'
-    middle_part = [alphabet[i] for i in middle_nums] # буквы по индексам
+    middle_part = [ALPHABET[i] for i in middle_nums] # буквы по индексам
     key = f'{frst_prt} {"".join(middle_part)} {last_prt}'
 
     return key
 
 
 def init_frames(root):
-    height = 500 # размеры окна
-    width = 600
-
-    lbl_height = 40 # размеры лейбла где будет ключ
-    lbl_width = 200
-    lbl_pad_y = 90 # отступ для лейбла сверху
-
-    gif_height = 135 # размеры гиф
-    gif_width = 185
-    gif_pad_x = 2 # отступы для нее
-    gif_pad_y = height - gif_height - gif_pad_x
-
-    btn_height = 50 # размеры кнопки
-    btn_width = 150
-    btn_pad_y = gif_pad_y # отступ для кнопки сверху
-
-    text_pad_x = 120
-    text_pad_y = 10 # отступы для текста 
 
     generated_key = tk.StringVar() # класс позволяет реализовать
     # динамическое управление переменными
@@ -56,14 +63,14 @@ def init_frames(root):
     main_frame = tk.Frame(root)
     main_frame.pack(fill=tk.BOTH, expand=True)
 
-    bg = tk.PhotoImage(file="acnh_bg.png") # задний фон
-    bg_canvas = tk.Canvas(main_frame, width=width, height=height)
+    bg = tk.PhotoImage(file=BG_FILE) # задний фон
+    bg_canvas = tk.Canvas(main_frame, width=WIDTH, height=HEIGHT)
     bg_canvas.pack(fill=tk.BOTH, expand=True)
     bg_canvas.create_image(0, 0, anchor=tk.NW, image=bg)
     bg_canvas.image = bg # чтобы картинка после выхода из функции оставалась 
 
     def click():
-        playsound("effect.mp3", False)
+        playsound(EFFECT_SOUND_FILE, False)
         time.sleep(0.5)
         generated_key.set(generate_key()) # задаем в переменную новое
         # сгенерированное значение
@@ -74,12 +81,12 @@ def init_frames(root):
                          borderwidth=1,
                          relief=tk.GROOVE,
                          background="#FFF8DC")
-    bg_canvas.create_window((width - lbl_width)//2, # добавляем в окно на фон
-                            lbl_pad_y, 
+    bg_canvas.create_window((WIDTH - LBL_WIDTH)//2, # добавляем в окно на фон
+                            LBL_PAD_Y, 
                             anchor=tk.NW, 
                             window=key_label, 
-                            width=lbl_width, 
-                            height=lbl_height)
+                            width=LBL_WIDTH, 
+                            height=LBL_HEIGHT)
     start_button = tk.Button(main_frame, # делаем кнопку
                              text="Сгенерировать",
                              font="Impact 15",
@@ -87,27 +94,26 @@ def init_frames(root):
                              foreground="#FFF8DC",
                              command=click,
                              cursor="heart")
-    bg_canvas.create_window((width - btn_width)//2, # добавляем кнопку 
+    bg_canvas.create_window((WIDTH - BTN_WIDTH)//2, # добавляем кнопку 
                             btn_pad_y,
                             anchor=tk.NW,
                             window=start_button,
-                            width=btn_width,
-                            height=btn_height)
-    bg_canvas.create_text(text_pad_x, 
-                          text_pad_y, # добавляем текст
+                            width=BTN_WIDTH,
+                            height=BTN_HEIGHT)
+    bg_canvas.create_text(TEXT_PAD_X, 
+                          TEXT_PAD_Y, # добавляем текст
                           text="Бесплатные ключи Animal Crossing",
                           font="Impact 18",
                           anchor=tk.NW)
     
-    gif_file = "punchyandbob.gif" # делаем гифку
-    gif = Image.open(gif_file)
+    gif = Image.open(GIF_FILE) # делаем гифку
     gif_label = tk.Label(main_frame, # лейбл для гифки
                          image="",
                          background="#008000")
     gif_frames = gif.n_frames # берем количество кадров в гифке
     gif_obj = [] 
     for i in range(gif_frames): # каждый кадр добавляем в список
-         obj = tk.PhotoImage(file=gif_file, format=f"gif -index {i}")
+         obj = tk.PhotoImage(file=GIF_FILE, format=f"gif -index {i}")
          gif_obj.append(obj)
     
     def animation(current_frame=0): # функция с рекурсией
@@ -122,28 +128,26 @@ def init_frames(root):
 
     animation()
 
-    bg_canvas.create_window(gif_pad_x, # добавляем гифку
+    bg_canvas.create_window(GIF_PAD_X, # добавляем гифку
                             gif_pad_y,
                             window=gif_label,
                             anchor=tk.NW,
-                            width=gif_width,
-                            height=gif_height,)
+                            width=GIF_WIDTH,
+                            height=GIF_HEIGHT,)
     return main_frame
 
 
 def init_gui():
     root = tk.Tk()
     root.title("Tom Nook's keygen") # название окна и иконка
-    icon = tk.PhotoImage(file="acnh_bells.png")
+    icon = tk.PhotoImage(file=ICON_FILE)
     root.iconphoto(False, icon)
 
-    height = 500 # размеры окна
-    width = 600
     screen_height = root.winfo_screenheight() # размеры экрана
     screen_width = root.winfo_screenwidth()
-    x_offset = (screen_width-width)//2
-    y_offset = (screen_height-height)//2 # размещаем окно посередине экрана
-    root.geometry(f"{width}x{height}+{x_offset}+{y_offset}")
+    x_offset = (screen_width-WIDTH)//2
+    y_offset = (screen_height-HEIGHT)//2 # размещаем окно посередине экрана
+    root.geometry(f"{WIDTH}x{HEIGHT}+{x_offset}+{y_offset}")
     root.resizable(False, False) # нельзя менять размеры окна
 
     init_frames(root)
@@ -151,7 +155,7 @@ def init_gui():
 
 
 def play_music(): # функция для воспроизведения музыки
-        playsound("bubblegum k.k..mp3")
+        playsound(MUSIC_FILE)
 
 
 if __name__ == "__main__":
